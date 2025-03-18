af3818e972a3b1dc4a7ca4d2ccac61792b7711c9
MDax <maximederenes@gmail.com>
2025-03-18 16:01:54 +0100

    Ajout du TP Connexion 1
    Ajout du TP Connexion 1

---
+++ TPCONNEXION1/tp_database.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 17 mars 2025 à 15:47
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `tp_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE `utilisateurs` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `age` int(11) NOT NULL,
+  `mdp` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `nom`, `email`, `age`, `mdp`) VALUES
+(7, 'Patrick', 'patrick@gmail.com', 23, 'AEDR23*'),
+(8, 'Henry', 'henry@gmail.com', 46, 'AEAEGVRA23'),
+(9, 'Messi', 'MessiGoat@hotmail.fr', 32, 'BGYTJ5'),
+(10, 'Maxime', 'maximederenes@gmail.com', 18, 'TIMIOUP5'),
+(11, 'Pablo', 'pabloecscobar@gmail.com', 67, 'Pablo19');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
